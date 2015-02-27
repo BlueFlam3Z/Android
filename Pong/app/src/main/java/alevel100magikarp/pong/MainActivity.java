49760be7028d7fad0d51e30e8eaefa31665e92e0
ArthurBF <arthurmw2@gmail.com>
2015-02-27 00:23:51 -0300

    Chupa meu pau daniel

--- Pong/app/src/main/java/alevel100magikarp/pong/MainActivity.java
+++ Pong/app/src/main/java/alevel100magikarp/pong/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
